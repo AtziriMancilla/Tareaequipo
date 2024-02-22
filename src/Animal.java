bf30e90007b87f59a617ff1336ebbc634bead5e0
Atziri Mancilla <atzirimc22@gmail.com>
2024-02-22 16:27:12 -0600

    Se subio la clase animal

---
+++ src/Animal.java
@@ -0,0 +1,11 @@
+public class Animal{
+    String nombre, especie;
+    int edad;
+
+    public Animal(String nombre, String especie, int edad){
+        this.nombre=nombre;
+        this.especie=especie;
+        this.edad=edad;
+    }
+    //crear metodos de comer, dormir, hibernar...
+}
